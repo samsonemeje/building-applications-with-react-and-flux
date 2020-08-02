bb62c270ca79eb7768baaeb238d9d32d6de4e4d5
Samson Emeje <samsonemeje@gmail.com>
2020-08-02 15:52:43 +0100

    remove initial about redirect button

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Header from './common/Header';
 import CoursesPage from './CoursesPage';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import NotFoundPage from './NotFoundPage';
 import ManageCoursePage from './ManageCoursePage';
 import { ToastContainer } from 'react-toastify';
@@ -20,7 +20,6 @@ function App() {
         <Route path='/about' component={AboutPage} />
         <Route path='/course/:slug' component={ManageCoursePage} />
         <Route path='/course' component={ManageCoursePage} />
-        <Redirect from='/about-page' to='about' />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
     <div className='jumbotron'>
-      <h1>Pluralsight Administration</h1>
+      <h1>Turtle Dove Administration</h1>
       <p>React, Flux, and React Router for ultra-responsive web apps.</p>
-      <Link to='about' className='btn btn-primary'>
-        About
-      </Link>
     </div>
   );
 }
